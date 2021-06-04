ed307998335f8129ffded7ad904b807f4826e0b7
aDaichiOta <daichi.ota@access-company.com>
2021-06-04 10:06:29 +0900

    [228886] Create CDK for S3 (#5)

--- serverCdk/lib/server_cdk-stack.ts
+++ serverCdk/lib/server_cdk-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as s3 from '@aws-cdk/aws-s3'
 
 export class ServerCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const env = this.node.tryGetContext('env') || 'dev';
 
     // cdk lambda-apigateway example
     const getHelloMessageLambda = new lambda.Function(this, 'getHelloMessageLambda', {
@@ -20,5 +23,18 @@ export class ServerCdkStack extends cdk.Stack {
     const helloMessageApi = restApi.root.addResource('helloMessage')
 
     helloMessageApi.addMethod('GET', new apigateway.LambdaIntegration(getHelloMessageLambda))
+
+    // S3 Buckets
+    const s3Props = {
+      enforceSSL: true,
+    }
+    const _logBucket = new s3.Bucket(this, 'logs', {
+      bucketName: `powergs-${env}-logs`,
+      ...s3Props
+    });
+    const _emsBucket = new s3.Bucket(this, 'ems', {
+      bucketName: `powergs-${env}-ems`,
+      ...s3Props
+    });
   }
 }
--- serverCdk/test/server_cdk.test.ts
+++ serverCdk/test/server_cdk.test.ts
@@ -2,10 +2,25 @@ import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@a
 import * as cdk from '@aws-cdk/core';
 import * as ServerCdk from '../lib/server_cdk-stack';
 
-test('ServerCdk contain lambda resource', () => {
+test('ServerCdk contains some resources', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new ServerCdk.ServerCdkStack(app, 'MyTestStack');
   // THEN
-  expectCDK(stack).to(haveResource('AWS::Lambda::Function', { Runtime: "nodejs14.x" }))
+  expectCDK(stack).to(haveResource('AWS::Lambda::Function', { Runtime: "nodejs14.x" }));
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket', { BucketName: 'powergs-dev-logs' }));
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket', { BucketName: 'powergs-dev-ems' }));
 });
+
+test('ServerCdk accepts env context', () => {
+  const app = new cdk.App({
+    context: {
+      env: 'prod',
+    }
+  });
+  // WHEN
+  const stack = new ServerCdk.ServerCdkStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket', { BucketName: 'powergs-prod-logs' }));
+  expectCDK(stack).to(haveResource('AWS::S3::Bucket', { BucketName: 'powergs-prod-ems' }));
+})
