e7f7531cc336313cfaea07d90905490c6cca0622
Stanley Liang <stanley.liang@access-company.com>
2021-06-11 10:12:40 +0800

    context menu of treeview (#32)

--- web/src/components/LeftDrawer/index.tsx
+++ web/src/components/LeftDrawer/index.tsx
@@ -1,3 +1,4 @@
+import { useState, MouseEvent } from 'react'
 import useStyles from './style'
 import Drawer from '@material-ui/core/Drawer'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -5,9 +6,36 @@ import TreeView from '@material-ui/lab/TreeView'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import TreeItem from '@material-ui/lab/TreeItem'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Box from '@material-ui/core/Box'
+
+interface MenuAnchorPosition {
+  mouseX: number | null
+  mouseY: number | null
+}
 
 const LeftDrawer = () => {
   const classes = useStyles()
+  const [state, setState] = useState<MenuAnchorPosition>({
+    mouseX: null,
+    mouseY: null,
+  })
+  const [selectedNodeId, setSelectedNodeId] = useState<string | null>(null)
+  const onContextMenu = (event: MouseEvent, nodeId: string | null) => {
+    event.preventDefault()
+    event.stopPropagation()
+    setSelectedNodeId(nodeId)
+    setState({
+      mouseX: event.clientX - 2,
+      mouseY: event.clientY - 4,
+    })
+  }
+
+  const handleClose = () => {
+    setState({ mouseX: null, mouseY: null })
+  }
+
   return (
     <Drawer
       className={classes.drawer}
@@ -16,24 +44,46 @@ const LeftDrawer = () => {
         paper: classes.drawerPaper,
       }}>
       <Toolbar />
-      <TreeView
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}>
-        <TreeItem nodeId="1" label="Applications">
-          <TreeItem nodeId="2" label="Calendar" />
-          <TreeItem nodeId="3" label="Chrome" />
-          <TreeItem nodeId="4" label="Webstorm" />
-        </TreeItem>
-        <TreeItem nodeId="5" label="Documents">
-          <TreeItem nodeId="10" label="OSS" />
-          <TreeItem nodeId="6" label="Material-UI">
-            <TreeItem nodeId="7" label="src">
-              <TreeItem nodeId="8" label="index.js" />
-              <TreeItem nodeId="9" label="tree-view.js" />
+      <Box p={2}>
+        <TreeView
+          defaultCollapseIcon={<ExpandMoreIcon />}
+          defaultExpandIcon={<ChevronRightIcon />}>
+          <TreeItem
+            nodeId="1"
+            label="Applications"
+            onContextMenu={(event) => onContextMenu(event, 'Applications')}>
+            <TreeItem
+              nodeId="4"
+              label="Webstorm"
+              onContextMenu={(event) => onContextMenu(event, 'Webstorm')}>
+              <TreeItem
+                nodeId="7"
+                label="src"
+                onContextMenu={(event) => onContextMenu(event, 'src')}>
+                <TreeItem
+                  nodeId="9"
+                  label="tree-view.js"
+                  onContextMenu={(event) =>
+                    onContextMenu(event, 'tree-view.js')
+                  }
+                />
+              </TreeItem>
             </TreeItem>
           </TreeItem>
-        </TreeItem>
-      </TreeView>
+        </TreeView>
+      </Box>
+      <Menu
+        keepMounted
+        open={state.mouseY !== null}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          state.mouseY && state.mouseX
+            ? { top: state.mouseY, left: state.mouseX }
+            : undefined
+        }>
+        <MenuItem onClick={handleClose}>{selectedNodeId}</MenuItem>
+      </Menu>
     </Drawer>
   )
 }
