ea533e19efaa01416dcbcb6e6c349ccb84f25e2b
aAnhVu <anh.vu@access-company.com>
2021-06-14 17:46:40 +0900

    using camel case in css

--- web/src/containers/Login/style.ts
+++ web/src/containers/Login/style.ts
@@ -2,30 +2,30 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   title: {
-    'margin-right': '1rem',
+    marginRight: '1rem',
     width: '35%',
   },
   inputItem: {
     display: 'flex',
-    'margin-top': '0.2rem',
-    'margin-bottom': '0.2rem',
+    marginTop: '0.2rem',
+    marginBottom: '0.2rem',
     width: '30%',
   },
   gridItem: {
     display: 'flex',
-    'flex-direction': 'column',
-    'align-items': 'center',
-    'justify-items': 'center',
-    'margin-top': '1rem',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyItems: 'center',
+    marginTop: '1rem',
   },
   buttonArea: {
     display: 'flex',
-    'margin-top': '1rem',
-    'justify-items': 'center',
+    marginTop: '1rem',
+    justifyItems: 'center',
   },
   forgotLink: {
     alignItems: 'center',
-    'margin-top': '1rem',
+    marginTop: '1rem',
   },
 })
 
--- web/src/containers/PasswordSetting/style.ts
+++ web/src/containers/PasswordSetting/style.ts
@@ -2,26 +2,26 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   title: {
-    'margin-right': '1rem',
+    marginRight: '1rem',
     width: '60%',
   },
   inputItem: {
     display: 'flex',
-    'margin-top': '0.2rem',
-    'margin-bottom': '0.2rem',
+    marginTop: '0.2rem',
+    marginBottom: '0.2rem',
     width: '40%',
   },
   gridItem: {
     display: 'flex',
-    'flex-direction': 'column',
-    'align-items': 'center',
-    'justify-items': 'center',
-    'margin-top': '1rem',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyItems: 'center',
+    marginTop: '1rem',
   },
   buttonArea: {
     display: 'flex',
-    'margin-top': '1rem',
-    'justify-items': 'center',
+    marginTop: '1rem',
+    justifyItems: 'center',
   },
 })
 
