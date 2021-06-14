0a17a07e3491f130db1e7a735e2981a605ec806a
aAnhVu <anh.vu@access-company.com>
2021-06-14 17:53:31 +0900

    using camel case in css

--- web/src/containers/PasswordChangeEmail/style.ts
+++ web/src/containers/PasswordChangeEmail/style.ts
@@ -3,38 +3,38 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles({
   label: {
     display: 'flex',
-    'flex-direction': 'column',
-    'margin-top': '0.2rem',
-    'margin-bottom': '0.2rem',
+    flexDirection: 'column',
+    marginTop: '0.2rem',
+    marginBottom: '0.2rem',
     width: '30%',
     '& p': {
       width: '100%',
     },
     '& h2': {
-      'text-align': 'center',
+      textAlign: 'center',
     },
   },
   inputItem: {
     display: 'flex',
-    'margin-top': '0.2rem',
-    'margin-bottom': '0.2rem',
+    marginTop: '0.2rem',
+    marginBottom: '0.2rem',
     width: '30%',
     '& p': {
-      'margin-right': '1rem',
+      marginRight: '1rem',
       width: '35%',
     },
   },
   gridItem: {
     display: 'flex',
-    'flex-direction': 'column',
-    'align-items': 'center',
-    'justify-items': 'center',
-    'margin-top': '1rem',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyItems: 'center',
+    marginTop: '1rem',
   },
   buttonArea: {
     display: 'flex',
-    'margin-top': '1rem',
-    'justify-items': 'center',
+    marginTop: '1rem',
+    justifyItems: 'center',
   },
 })
 
